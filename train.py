5b0e5c3c1b4f07304834d96426a6b947f0e73f5f
Ipsedo <berriens@gmail.com>
2021-01-24 02:42:12 +0100

    add train script

---
+++ train.py
@@ -0,0 +1,66 @@
+from audio import WavDataset, to_wav
+from networks import WaveNet
+from generate import generate_from
+
+import torch as th
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from tqdm import tqdm
+from glob import glob
+
+
+def main() -> None:
+    n_class = 256
+    n_block = 2
+    n_layer = 9
+    hidden_channel = 32
+
+    wavenet = WaveNet(
+        n_block, n_layer, 1, hidden_channel, n_class
+    )
+
+    wavenet.cuda()
+
+    optim = th.optim.Adam(wavenet.parameters(), lr=1e-4)
+    criterion = nn.CrossEntropyLoss()
+
+    files = glob("./res/test_16000Hz/*.flac")
+
+    data_set = WavDataset(files, n_class, 16000, 32000, True)
+
+    data_loader = DataLoader(data_set, batch_size=4, shuffle=True)
+
+    nb_epoch = 10
+
+    for e in range(nb_epoch):
+
+        tqdm_bar = tqdm(data_loader)
+        for x, y in tqdm_bar:
+            x = x.cuda()
+            y = y.cuda()
+
+            out = wavenet(x)
+
+            loss = criterion(out, y)
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            tqdm_bar.set_description(f"Epoch {e}, loss = {loss.item()}")
+
+        n_sec_gen = 2
+        n_sample = 16000 * n_sec_gen
+        init = th.rand(100).cuda() * 2. - 1.
+
+        generated_sound = generate_from(wavenet, n_sample, init)
+
+        to_wav(
+            generated_sound.numpy(),
+            16000, f"./res/gen_epoch_{e}.wav"
+        )
+
+
+if __name__ == '__main__':
+    main()
